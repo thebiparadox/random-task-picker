010e2adba6a6ed447833a377564993dc90d5b652
thebiparadox <swehlatessa@gmail.com>
2024-07-15 11:27:21 -0400

    added psuedocode notes for functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// get elements from DOM //
+
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const taskForm = document.getElementById("task-form");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
@@ -7,3 +9,21 @@ const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
 const tasksContainer = document.getElementById("tasks=container");
+const randomizeTasksContainer = document.getElementById("randomize-tasks-container");
+const randomizeTasksBtn = document.getElementById("randomize-tasks-btn");
+const randomTaskOutput = document.getElementById("random-task-output");
+
+// create tasks //
+
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
+let currentTask = {};
+
+const addOrUpdateTask 
+
+// edit or delete tasks //
+
+// randomize tasks //
+
+// random task output //
+
+// button functionality //
