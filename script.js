b99b7a03d3c1d4c3fab0c2989f25fe9bd4d4d048
thebiparadox <swehlatessa@gmail.com>
2024-07-17 10:27:14 -0400

    Refactored addOrUpdateTask and updateTaskContainer Refactored addOrUpdateTask and updateTaskContainer into two functions

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
-const tasksContainer = document.getElementById("tasks=container");
+const tasksContainer = document.getElementById("tasks-container");
 const randomizeTasksContainer = document.getElementById("randomize-tasks-container");
 const randomizeTasksBtn = document.getElementById("randomize-tasks-btn");
 const randomTaskOutput = document.getElementById("random-task-output");
@@ -18,12 +18,38 @@ const randomTaskOutput = document.getElementById("random-task-output");
 
 const taskData = [];
 let currentTask = {};
+
+const addOrUpdateTask = () => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    taskObj = {
+        id: `${taskTitleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        taskTitle: taskTitleInput.value,
+        taskDescription: taskDescriptionInput.value,
+    };
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    }
+};
+
+const updateTaskContainer = () => {
+    taskData.forEach(({ id, taskTitle, taskDescription }) => {
+        tasksContainer.innerHTML += `
+        <div id="${id}" class="task">
+        <p><strong>Title:</strong> ${taskTitle}</p>
+        <p><strong>Description:</strong> ${taskDescription}</p>
+        <button class="btn" type="button">Edit</button>
+        <button class="btn" type="button">Delete</button>
+        </div>
+        `
+    })
+}
+
 const reset = () => {
-    taskTitleInput = "";
-    taskDescriptionInput = "";
+    taskTitleInput.value = "";
+    taskDescriptionInput.value = "";
     taskForm.classList.toggle("hidden");
     currentTask = {};
-}
+};
 
 // edit or delete tasks //
 
@@ -32,8 +58,9 @@ const reset = () => {
 // random task output //
 
 // button functionality //
-openTaskFormBtn.addEventListener("click", (e) => taskForm.classList.toggle("hidden"));
-closeTaskFormBtn.addEventListener("click", (e) => {
+openTaskFormBtn.addEventListener("click", () => taskForm.classList.toggle("hidden"));
+
+closeTaskFormBtn.addEventListener("click", () => {
     const formInputsContainValues = taskTitleInput.value || taskDescriptionInput.value;
     if (formInputsContainValues) {
         confirmCloseDialog.showModal();
@@ -41,31 +68,15 @@ closeTaskFormBtn.addEventListener("click", (e) => {
         reset();
     }
 });
-cancelBtn.addEventListener("click", (e) => confirmCloseDialog.close());
-discardBtn.addEventListener("click", (e) => {
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
     reset();
 });
+
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    taskObj = {
-        taskId: `${taskTitleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        taskTitle: `${taskTitleInput.value}`,
-        taskDescription: `${taskDescriptionInput.value}`,
-    };
-    if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-    }
-    taskData.forEach(({ taskId, taskTitle, taskDescription }) => {
-        tasksContainer.innerText += `
-        <div id="${taskId}" class="task">
-        <p><strong>Title:</strong> ${taskTitle}</p>
-        <p><strong>Description:</strong> ${taskDescription}</p>
-        <button class="btn" type="button">Edit</button>
-        <button class="btn" type="button">Delete</button>
-        </div>
-        `;
-    })
     reset();
 });
